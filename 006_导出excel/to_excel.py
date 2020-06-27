48fffed9eeeecb711fdb8d9dbb7a888e04b298c2
dxp432 <dxp432@163.com>
2020-06-27 22:23:16 +0800

    导出excel

---
+++ 006_导出excel/to_excel.py
@@ -0,0 +1,11 @@
+# 第1步：一定是先导入我们的库
+import pandas as pd
+
+# 第2步：读取表
+df_my_books = pd.read_excel('books.xlsx', sheet_name='Sheet1')
+
+# 第3步：显示出来
+print(df_my_books)
+
+# 第4步，导出
+df_my_books.to_excel('newbooks.xlsx',index = False)
